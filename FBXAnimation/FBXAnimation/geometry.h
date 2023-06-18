33f8047fd786d46b663fd589959b132ed81f4f5d
Chris <chrisrkemp@hotmail.co.uk>
2023-06-18 15:31:50 +0100

    Added quaternion ops

--- FBXAnimation/FBXAnimation/geometry.h
+++ FBXAnimation/FBXAnimation/geometry.h
@@ -56,6 +56,12 @@ namespace geom
         return result;
     }
 
+    Vector3 operator*(const Quaternion& q, Vector3 vec)
+    {
+        Quaternion q_res = q * Quaternion{ vec.x, vec.y, vec.z, 0.f } *q.inverse();
+        return { q_res.x, q_res.y, q_res.z };
+    }
+
     //conversions/constructions
 
     Matrix34 create_translation_matrix_34(Vector3 vec)
--- FBXAnimation/FBXAnimation/quaternion.h
+++ FBXAnimation/FBXAnimation/quaternion.h
@@ -13,7 +13,55 @@ namespace geom
 
         static Quaternion identity() { return { 0,0,0,1.f }; }
         Quaternion normalized() const;
+        Quaternion inverse() const;
+        float angle() const;
     };
+
+    //inline operator definitions
+
+    inline Quaternion operator+(const Quaternion& lhs, const Quaternion& rhs)
+    {
+        return { 
+            lhs.x + rhs.x,
+            lhs.y + rhs.y,
+            lhs.z + rhs.z,
+            lhs.w + rhs.w
+        };
+    }
+
+    inline Quaternion operator*(const Quaternion& lhs, const Quaternion& rhs)
+    {
+        //lhs.x * i * rhs.(xi, yj, zk, w)
+        Quaternion qx = {
+            lhs.x * rhs.w,
+            -lhs.x * rhs.z,
+            lhs.x * rhs.y,
+            -lhs.x * rhs.x
+        };
+        //lhs.y * j * rhs.(xi, yj, zk, w)
+        Quaternion qy = {
+            lhs.y * rhs.z,
+            lhs.y * rhs.w,
+            -lhs.y * rhs.x,
+            -lhs.y * rhs.y
+        };
+        //lhs.z * k * rhs.(xi, yj, zk, w)
+        Quaternion qz = {
+            -lhs.z * rhs.y,
+            lhs.z * rhs.x,
+            lhs.z * rhs.w,
+            -lhs.z * rhs.x
+        };
+        //lhs.w * rhs.(xi, yj, zk, w)
+        Quaternion qw = {
+            lhs.w * rhs.x,
+            lhs.w * rhs.y,
+            lhs.w * rhs.z,
+            lhs.w * rhs.w
+        };
+
+        return qx + qy + qz + qw;
+    }
 
     //inline definitions
 
@@ -31,5 +79,15 @@ namespace geom
     {
         float scaling = 1.f / sqrtf(x * x + y * y + z * z + w * w);
         return { x * scaling, y * scaling, z * scaling, w * scaling };
+    }
+
+    inline Quaternion Quaternion::inverse() const
+    {
+        return { -x, -y, -z, w };
+    }
+
+    inline float Quaternion::angle() const
+    {
+        return 2 * acosf(w);
     }
 }
